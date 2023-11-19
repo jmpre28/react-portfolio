71c12285ebf5deb5114efefc4b87c9eaf43418f4
Jake Preciado <jacobpreciado@gmail.com>
2023-11-19 12:57:04 -0700

    App file/'home page'

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+export default function App() {
+  return (    
+  <div className="col-10">
+    <h2>About Me</h2>                
+    <p>
+    Hello and welcome to my portfolio! I'm Jake Preciado, a passionate learner on the journey to becoming a full stack web engineer. 
+    This space is dedicated to showcasing my evolving skills and experiences in the world of web development.
+    </p>
+    <p>
+    In the ever-evolving field of web development, I find joy in crafting seamless and visually appealing digital 
+    experiences. From front-end aesthetics to back-end functionality, each line of code represents a step closer to my goal 
+    of creating impactful and user-friendly websites.
+    </p>
+    <p>
+    Outside of coding, you'll often find me outdoors with my camera or drone, capturing the world around me. And 
+    when I'm not doing that, I'm jamming on my guitar, creating music that brings a little extra joy to my day. 
+    </p>
+  </div>     
   )
 }
-
-export default App
